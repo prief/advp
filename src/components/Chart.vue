125522c2d89ff44b0eb28c789a7c8380b33defab
prief <207792089@qq.com>
2019-07-26 23:50:56 +0800

    add echarts

---
+++ src/components/Chart.vue
@@ -0,0 +1,55 @@
+<template>
+  <div ref="echarts"></div>
+</template>
+
+<script>
+import echarts from 'echarts';
+import { addListener, removeListener } from 'resize-detector';
+import debounce from 'lodash/debounce';
+export default {
+  props: {
+    option: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  created() {
+    this.resize = debounce(this.resize, 300);
+  },
+  mounted() {
+    this.renderChart();
+    addListener(this.$refs.echarts, this.resize);
+    // 指定图表的配置项和数据
+  },
+  watch: {
+    // 深度监听比较耗性能
+    // option: {
+    //   handler(v) {
+    //     this.myChart.setOption(v);
+    //   },
+    //   deep: true,
+    // },
+    option(v) {
+      this.myChart.setOption(v);
+    },
+  },
+  methods: {
+    resize() {
+      console.log('resize');
+      this.myChart.resize();
+    },
+    renderChart() {
+      this.myChart = echarts.init(this.$refs.echarts);
+      // 使用刚指定的配置项和数据显示图表。
+      this.myChart.setOption(this.option);
+    },
+  },
+  beforeDestroy() {
+    removeListener(this.$refs.echarts, this.resize);
+    this.myChart.dispose();
+    this.myChart = null;
+  },
+};
+</script>
+
+<style></style>
--- src/views/dashboard/Analysis.vue
+++ src/views/dashboard/Analysis.vue
@@ -1,9 +1,52 @@
 <template>
-  <div>analysis</div>
+  <div>
+    analysis
+    <Charts style="height:400px" :option="opt" />
+  </div>
 </template>
 
 <script>
-export default {};
+import Charts from '../../components/Chart';
+import random from 'lodash/random';
+export default {
+  components: {
+    Charts,
+  },
+  mounted() {
+    this.timer = setInterval(() => {
+      this.opt.series[0].data = this.opt.series[0].data.map(() => random(100));
+
+      this.opt = { ...this.opt };
+    }, 2000);
+  },
+  destroyed() {
+    clearTimeout(this.timer);
+  },
+  data() {
+    return {
+      opt: {
+        title: {
+          text: 'ECharts 入门示例',
+        },
+        tooltip: {},
+        legend: {
+          data: ['销量'],
+        },
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20],
+          },
+        ],
+      },
+    };
+  },
+};
 </script>
 
 <style></style>
